0488711486f35f6cfa321100005649374208259f
wonejito <30444850+wonejito@users.noreply.github.com>
2023-03-06 21:38:09 -0400

    Today was a good day

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
 let playground = document.querySelector("#dirty");
 let toposquare = document.querySelectorAll(".toposquare");
 let title = document.querySelector("#title");
+let start = document.querySelector("#start");
+let stop = document.querySelector("#stop");
 let topo = document.createElement("img");
 topo.src = "/images/red-dot.png";
-topo.style.cssText = "width: 0.5em; height; 0.5em";
+topo.style.cssText = "width: 0.9em; height; 0.9em";
 
-toposquare.forEach((cuadro) => {
-	cuadro.addEventListener("click", function () {
-		topoAttack(this);
-	});
+// toposquare.forEach((cuadro) => {
+// 	cuadro.addEventListener("click", function () {
+// 		topoAttack(this);
+// 	});
+// });
+
+// function topoAttack(elemnt) {
+// 	elemnt.appendChild(topo);
+// }
+
+let letsPlay = setInterval(function () {
+	topoAppears(toposquare);
+}, 2000);
+
+start.addEventListener("click", function () {
+	letsPlay;
 });
 
-function topoAttack(elemnt) {
-	elemnt.appendChild(topo);
-}
+stop.addEventListener("click", function () {
+	clearTimeout(letsPlay);
+});
 
-// title.appendChild(topo);
+function topoAppears(item) {
+	item = [...item];
+	let random = Math.floor(Math.random() * item.length);
+	item[random].appendChild(topo);
+}
